edba256547c38660693bf701f745782514d7894e
Davi Andrade Vital Ribeiro <142036466+vitaldavi@users.noreply.github.com>
2024-05-14 16:53:18 -0300

    Update Introdução.py

--- Introdução.py
+++ Introdução.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import yfinance as yf
-import plotly.express as px
 
 st.set_page_config(page_title="Gasset Dashboard")
 st.title("Gasset Dashboard")
@@ -10,8 +6,6 @@ st.subheader("Nós apresentamos a nossa dashboard que compila dados de ações e
 st.write("""Deseja-se buscar dados de ações, criando histogramas de retornos e outras estatísticas descritivas, 
          dados macroeconômicos como inflação, curva de juros, preços de commodities etc.""")
 st.caption("Criador: Davi Andrade")
-
-
 
 st.subheader("Informações Importantes")
 st.write("Os dados aqui apresentados possuem uma série de fontes, que estão elencadas a seguir. Os códigos são de autoria própria.")
